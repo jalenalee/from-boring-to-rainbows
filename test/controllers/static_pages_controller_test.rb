022afbfc70fbef5289fd7e7d11cba70aeabe1738
Jalena Lee <jalenalee@gmail.com>
2020-02-03 14:34:16 -0500

    FIXED TESTS

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,15 +2,14 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
-    assert_select "title", "About | Jalena
-    s Blog"
+    assert_select "title", "Jalena'a Blob"
   end
 
   test "should get root" do
     get root_url
     assert_response :success
   end
-  
+
 end
